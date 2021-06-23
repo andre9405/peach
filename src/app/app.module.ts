2dc9eade64ffc3ac6bc1284a1b98d0e5de19a49b
andre9405 <andre@techsonic.co.za>
2021-06-23 11:12:40 +0200

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {DatePipe,CurrencyPipe} from  '@angular/common';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatTableModule } from '@angular/material/table';
+import { FormsModule,ReactiveFormsModule  } from '@angular/forms';
+import { transaction } from './transactions';
+import { MatFormFieldModule  } from '@angular/material/form-field';
+import  { MatPaginatorModule } from '@angular/material/paginator';
+import  {  MatInputModule} from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
+    
+    
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTableModule,
+    FormsModule,
+    ReactiveFormsModule ,
+    MatFormFieldModule,MatPaginatorModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    
   ],
-  providers: [],
+  providers: [DatePipe,CurrencyPipe
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
